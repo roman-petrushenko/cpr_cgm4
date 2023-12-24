2bf018d7ad046ca0dd20f49b9124dd9aa9b203ce
roman-petrushenko <118403605+roman-petrushenko@users.noreply.github.com>
2023-12-24 17:44:39 +0200

    Add files via upload

---
+++ centr_pr_cmp_Fixed.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+f = open("DS4.txt","r")
+i = 0
+a = list()
+b = list()
+while True:
+    c = list()
+    c = f.readline().split(" ")
+    if (c[0] == ""):
+        break
+    a.append(float(c[0]))
+    b.append(float(c[1].replace("\n","")))
+a2 = list()
+b2 = list()
+j = 0
+while (j<len(a)):
+    a2.append(960*(540-a[j])/(960-b[j]))
+    b2.append(96000/(960-b[j]))
+    j+=1
+plt.figure(figsize=(960/100, 540/100))
+plt.scatter(a2,b2)
+plt.xlabel("X1")
+plt.ylabel("Z")
+plt.savefig("cpr.png")
+plt.show()
